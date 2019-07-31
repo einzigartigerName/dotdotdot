019b80ae95ef81fb96722f3f886d883bff34d5ec
einzigartigerName <nelson.gillo@gmx.de>
2019-07-31 17:26:09 +0200

    Fix: brightness value now in polybar on startup

--- scripts/polybar/brightness.py
+++ scripts/polybar/brightness.py
@@ -5,8 +5,26 @@ import dbus
 from dbus.mainloop.glib import DBusGMainLoop
 import os
 
-def print_status(value):
-    os.system("echo {}%".format(value))
+# Max brightness for panel
+MAX_BRIGHTNESS = "/sys/class/backlight/intel_backlight/max_brightness"
+m_file = open(MAX_BRIGHTNESS)
+max = int(m_file.read())
+m_file.close()
+
+ACTUAL_BRIGHTNESS = "/sys/class/backlight/intel_backlight/actual_brightness"
+
+def print_status():
+    # actual value
+    a_file = open(ACTUAL_BRIGHTNESS)
+    actual = int(a_file.read())
+    a_file.close()
+
+    # calc brightness value
+    percent = int((actual * 100) / max)
+
+    # print
+    os.system("echo {}%".format(percent))
+    
     
 
 def monitor_notification(bus, message):
@@ -20,12 +38,14 @@ def monitor_notification(bus, message):
 
         # Check if Spotify
         if notification["app_name"] == "BRIGHTNESS":
-            print_status(notification["summary"])
+            print_status()
 
 
 loop = DBusGMainLoop(set_as_default=True)
 session_bus = dbus.SessionBus()
 session_bus.add_match_string("type='method_call',interface='org.freedesktop.Notifications',member='Notify',eavesdrop=true")
 session_bus.add_message_filter(monitor_notification)
+
+print_status()
 
 GLib.MainLoop().run()
